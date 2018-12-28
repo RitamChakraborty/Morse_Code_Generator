2b12aab2d41d9a28950d9cb943810770dd125ccc
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2018-12-28 15:56:21 +0530

    Converter class created

---
+++ src/MorseCodeGenerator.java
@@ -0,0 +1,11 @@
+public class MorseCodeGenerator {
+    private final String text;
+
+    public MorseCodeGenerator(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
