2371b52f40105b1742f0712d987f3fd160c4cc7c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2018-12-28 15:58:26 +0530

    Morse code container class created

---
+++ src/data/MorseCode.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseCode {
+    protected Map<Character, String> morseCodeList;
+
+    public MorseCode() {
+        morseCodeList = new HashMap<>();
+    }
+}
