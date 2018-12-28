382453e0abb2eaf890204238bb4701efa59e917d
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2018-12-28 15:54:25 +0530

    Main file created

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String string;
+
+        System.out.println("Enter text: ");
+        string = new Scanner(System.in).nextLine();
+    }
+}
