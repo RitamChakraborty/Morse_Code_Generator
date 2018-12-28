02188df08f2a25ac0c2906ee9845b01edbf9d1a0
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2018-12-28 16:01:00 +0530

    Implementation added to MorseCodeGenerator

--- src/MorseCodeGenerator.java
+++ src/MorseCodeGenerator.java
@@ -1,11 +1,38 @@
-public class MorseCodeGenerator {
+import data.MorseCode;
+
+import java.util.InputMismatchException;
+
+public class MorseCodeGenerator extends MorseCode {
     private final String text;
+    private StringBuilder morseCode;
+    private char[] charArray;
 
     public MorseCodeGenerator(String text) {
         this.text = text;
+        morseCode = new StringBuilder();
+    }
+
+    private void setCharArray() {
+        charArray = text.toCharArray();
+    }
+
+    private void setMorseCode() {
+        for (char ch: charArray) {
+            if (morseCodeList.containsKey(Character.valueOf(ch))) {
+                morseCode.append(morseCodeList.get(Character.valueOf(ch))).append(" ");
+            } else {
+                throw new InputMismatchException(ch + " is not found in Morse Code");
+            }
+        }
     }
 
     public String getText() {
         return text;
+    }
+
+    public String getMorseCode() {
+        setCharArray();
+        setMorseCode();
+        return morseCode.toString();
     }
 }
